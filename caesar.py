125f5ed23db67033b3d143f396d6aaaa160104df
Ayleen Roque <118084437+IcedPeppermintTea@users.noreply.github.com>
2023-10-31 01:08:34 -0400

    Add files via upload

---
+++ caesar.py
@@ -0,0 +1,38 @@
+"""
+Caesar Cypher Encryption Crack
+"""
+from string import printable # get the alphabet
+
+if __name__ == "__main__":
+    phrase = "Bsfo(u0zpv0b0mjuumf0tipsu0up0cf0b0tupsnusppqfs@"
+    key = 1
+    mode = "d" # encrypt or decrypt
+    codex = printable
+    codex += " " # add in spaces
+
+    transformed = ""
+
+    for i in range(len(codex)):  # print out every possible combination (since we do not know the key)
+        key += 1  # increment the key for however many characters we have (26)
+
+        for token in phrase:
+            if token in codex:
+                tokenidx = codex.find(token)
+
+                if mode == "e":
+                    transformidx = (tokenidx + key) % len(codex)
+                else:
+                    transformidx = (tokenidx - key) % len(codex)
+
+                transformed += codex[transformidx]
+
+
+            else:
+                transformed = transformed + token
+                print("the key: ")
+
+    print(transformed)
+    print("the key: ")
+
+
+
